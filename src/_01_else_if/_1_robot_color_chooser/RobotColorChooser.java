ee359cea71b7823b8bb759a9705ee9c4b65a48a0
league <57720048+richardson-liou@users.noreply.github.com>
2023-04-07 16:59:25 -0700

    zsegdfhd

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,18 +2,32 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot rob = new Robot();
+	    //2. Make the robot draw a shape (this will take more than one line of code)
+		
 		//3. Set the pen width to 10
+		rob.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
+		String Question = JOptionPane.showInputDialog("What Color Would You Like The Robot To Draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		for (int abcd = 0; abcd < 2; abcd++) {
+		
+		if (Question.equalsIgnoreCase("red")) { rob.setPenColor(255,0,0);}
+		if (Question.equalsIgnoreCase("green")) { rob.setPenColor(0,255,0);}
+		if (Question.equalsIgnoreCase("blue")) { rob.setPenColor(0,0,255);}
         //6. If the user doesn't enter anything, choose a random color
+		if (Question.equalsIgnoreCase(" ")) {rob.setRandomPenColor();}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		
 		
 
 		
@@ -21,4 +35,5 @@ public class RobotColorChooser {
 
 
 	}
+}
 }
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,14 +3,21 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String Question = JOptionPane.showInputDialog("How Many Cats Do You Have?");
 		// 2. Convert their answer into an int
+		
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+		
 		//    Hint: You can use the && operator 
+		
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 		
